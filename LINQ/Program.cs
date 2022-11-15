37f02b9e367016f61789eeb78ae46e09914fb816
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-11-15 18:18:44 -0500

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() { "God of War", "Call of Duty", "Assassins Creed", "Pac Man", "Super Mario Bros", "Donkey Kong" };
+            var Games = videoGames.OrderBy(game => game.Count());
+            foreach (var game in Games)
+            {
+                Console.WriteLine(game);
+            }
+            
         }
     }
 }
